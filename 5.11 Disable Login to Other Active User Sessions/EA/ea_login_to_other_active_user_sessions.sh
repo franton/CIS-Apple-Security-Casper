12a28280afb9be721d7b5a3ab784d47f4d824b82
Richard Purves <franton@users.noreply.github.com>
2016-07-29 11:55:37 +0200

    Update ea_login_to_other_active_user_sessions.sh
    Replaced previous detection method.

--- 5.11 Disable Login to Other Active User Sessions/EA/ea_login_to_other_active_user_sessions.sh
+++ 5.11 Disable Login to Other Active User Sessions/EA/ea_login_to_other_active_user_sessions.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-# CIS 5.11 - Curtesy of Owen Pragel (owen dot pragel @ 74bit dot com)
-# Minor modifications by Richard Purves (franton)
-# Note: i'm not entirely happy with this and the fix. Currently investigating authorizationdb method.
+# EA to check if other users can log into active user session via screensaver
 
-screensaverSecurity=$(grep -i "group=admin,wheel fail_safe" /etc/pam.d/screensaver)
+check=$( security authorizationdb read system.login.screensaver 2>/dev/null | grep "authenticate-session-user" | sed 's/<^>]*>//g' | sed 's/^[[:space:]]*//' )
 
-if [ "$screensaverSecurity" == "" ]; then
-    eaResult="Disabled"
+if [ "$check" == "authenticate-session-user" ]; then
+    echo "<result>Disabled</result>"
 else
-    eaResult="Enabled"
+    echo "<result>Enabled</result>"
 fi
-
-echo "<result>$eaResult</result>"

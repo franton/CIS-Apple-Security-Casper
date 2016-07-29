c299861c21204454616cd44106abb67fe3d08bd9
Richard Purves <franton@users.noreply.github.com>
2016-07-29 11:57:19 +0200

    Update and rename disable_login_to_other_active_user_sessions.sh to AccessActiveUserSession.sh

---
+++ 5.11 Disable Login to Other Active User Sessions/Config/AccessActiveUserSession.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# This is more reliable than previous version and probably better practice.
+
+security authorizationdb write system.login.screensaver authenticate-session-user
--- 5.11 Disable Login to Other Active User Sessions/Config/disable_login_to_other_active_user_sessions.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# CIS 5.11 - Curtesy of Owen Pragel (owen dot pragel @ 74bit dot com)
-
-sed -i.bak 's/group=admin,wheel fail_safe/group=wheel fail_safe/g' /etc/pam.d/screensaver

2fe40231d2cd346999e44f1ba826e0a12974451e
Richard Purves <franton@users.noreply.github.com>
2016-07-29 09:56:03 +0200

    Update disable_login_to_other_active_user_sessions.sh
    Attribution

--- 5.11 Disable Login to Other Active User Sessions/Config/disable_login_to_other_active_user_sessions.sh
+++ 5.11 Disable Login to Other Active User Sessions/Config/disable_login_to_other_active_user_sessions.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-# CIS 5.11
+# CIS 5.11 - Curtesy of Owen Pragel (owen dot pragel @ 74bit dot com)
+
 sed -i.bak 's/group=admin,wheel fail_safe/group=wheel fail_safe/g' /etc/pam.d/screensaver

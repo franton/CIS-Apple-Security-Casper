74f145a2a0659dfc4dead642c5b9a5b2048774aa
Richard Purves <contact@richard-purves.com>
2016-07-29 22:52:26 +0200

    Removed the user code as it wasn’t necessary

--- 2.3.1 Screen saver inactivity timer/EA/ScreenSaverInactivityTimerCheck.py
+++ 2.3.1 Screen saver inactivity timer/EA/ScreenSaverInactivityTimerCheck.py
@@ -3,15 +3,6 @@
 # EA to check the inactivity interval for the screen saver.
 # This will return the number of seconds, which you can scope a smart group from.
 
-# Get the current user of the computer.
-
-from SystemConfiguration import SCDynamicStoreCopyConsoleUser
-import sys
-username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]
-username = [username,""][username in [u"loginwindow", None, u""]]
-
-# Read the current user's screensaver byhost preference file
-
 import CoreFoundation
 domain = 'com.apple.screensaver'
 key = 'idleTime'

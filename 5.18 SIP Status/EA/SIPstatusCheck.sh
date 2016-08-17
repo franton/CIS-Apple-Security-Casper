ef063248be03bed363a31f69c96167ddf8bce279
Richard Purves <franton@users.noreply.github.com>
2016-08-17 16:03:37 +0200

    Update SIPstatusCheck.sh
    Typo fix.

--- 5.18 SIP Status/EA/SIPstatusCheck.sh
+++ 5.18 SIP Status/EA/SIPstatusCheck.sh
@@ -4,7 +4,7 @@ check=$( csrutil status )
 
 if [ "$check" = "System Integrity Protection status: enabled." ];
 then
-	echo "<result>Enabled</false>"
+	echo "<result>Enabled</result>"
 else
-	echo "<result>$check</false>"
+	echo "<result>$check</result>"
 fi

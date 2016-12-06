bbd5087f720a3445f98d5bd930c88c90d9de35e5
Richard Purves <contact@richard-purves.com>
2016-12-06 17:05:19 +0100

    SIP check update
    From 10.12.2 onwards, a reset of SIP settings back to “on” can be done
    from the main OS instead of just the recovery partition. This requires
    a reboot to take place.

---
+++ 5.18 Enable SIP/Config/EnableSIP.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Enable System Integrity Protection
+# Note: requires 10.12.2 onwards and a reboot for this to take effect. So only the command to do the work is here.
+
+sudo csrutil clear

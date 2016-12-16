f40257f0291b7783fb03086c124a748ea2b98202
Richard Purves <contact@richard-purves.com>
2016-12-16 13:27:26 +0100

    Code Improvements
    Simplifed menu bar check.

--- 4.2 WiFi Menu Bar/EA/WiFiMenuBarCheck.sh
+++ 4.2 WiFi Menu Bar/EA/WiFiMenuBarCheck.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-user=$( python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");' )
-read=$( defaults read /Users/$user/Library/Preferences/com.apple.systemuiserver.plist menuExtras | grep AirPort.menu )
+read=$( defaults read com.apple.systemuiserver.plist menuExtras | grep AirPort.menu )
 check=$( echo $read | cut -d '"' -f 2 | sed 's/;$//' )
 
 if [ "$check" = "/System/Library/CoreServices/Menu Extras/AirPort.menu" ];

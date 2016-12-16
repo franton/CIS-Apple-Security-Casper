6b80839b004c70002fedd0d7c05608d0d65cc711
Richard Purves <contact@richard-purves.com>
2016-12-16 14:04:06 +0100

    Reporting fail
    Fixed reporting case where no apps are present in the Application
    Firewall.

--- 2.6.5 How many apps in the AF/EA/NoOfAppsinAF.sh
+++ 2.6.5 How many apps in the AF/EA/NoOfAppsinAF.sh
@@ -4,4 +4,9 @@
 
 check=$( /usr/libexec/ApplicationFirewall/socketfilterfw --listapps | grep 'ALF: total number of apps' | awk '{ print $7 }' )
 
-echo "<result>$check</result>"
+if [ "$check" = "" ];
+then
+    echo "<result>0</result>"
+else
+    echo "<result>$check</result>"
+fi

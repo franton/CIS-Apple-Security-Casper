dd845cebe477acfe35811df4ccd3d4ec00f5dacc
Richard Purves <contact@richard-purves.com>
2016-12-22 16:39:10 +0000

    More bug fixes
    OCSP and CRL checking is a user level plist file. Fix for gatekeeper
    enabling.

--- 5.6 Enable OCSP and CRL certificate checking/Config/SetOCSPchecking.sh
+++ 5.6 Enable OCSP and CRL certificate checking/Config/SetOCSPchecking.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-defaults write com.apple.security.revocation OCSPStyle -string RequireIfPresent
+user=$( python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");' )
+
+defaults write /Users/$user/Library/Preferences/com.apple.security.revocation OCSPStyle -string RequireIfPresent

b6ceef6a69988aeda27f911c4fffe3ff24fe8fb2
Richard Purves <contact@richard-purves.com>
2016-12-22 21:32:33 +0000

    Reworked CRL/OCSP checking
    Unimpressed with the API failing to read a user level plist file. Had
    to use defaults and a full file path.

---
+++ 5.6 Enable OCSP and CRL certificate checking/EA/CheckRevocationFirst.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+user=$( python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");' )
+check=$( defaults read /Users/$user/Library/Preferences/com.apple.security.revocation RevocationFirst 2>&1 )
+
+if [ "$check" = "OCSP" ];
+then
+	echo "<result>Enabled</result>"
+else
+	echo "<result>Disabled</result>"
+fi

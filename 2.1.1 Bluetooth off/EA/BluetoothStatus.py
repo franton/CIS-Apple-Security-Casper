221552d59aeaea9049c21fb9071ae75c974fb582
Richard Purves <richard@richard-purves.com>
2017-01-27 14:00:05 +0100

    VM compatibility
    Checks for the presence of BT hardware before proceeding, or we get
    erroneous results.

--- 2.1.1 Bluetooth off/EA/BluetoothStatus.py
+++ 2.1.1 Bluetooth off/EA/BluetoothStatus.py
@@ -2,7 +2,15 @@
 
 # EA to check for Bluetooth Status
 
+import sys
+import subprocess
 import CoreFoundation
+
+bttest = subprocess.call(["system_profiler SPBluetoothDataType | grep -i discoverable | awk '{ print $2 }'"], shell=True )
+
+if bttest == 0:
+    print "<result>Missing Bluetooth Device</result>"
+    sys.exit()
 
 domain = 'com.apple.Bluetooth'
 key = 'ControllerPowerState'

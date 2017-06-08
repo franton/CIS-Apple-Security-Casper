7af128e0053453ba9eeadf689046932d61308e49
Richard Purves <richardpurves@Richards-MBP.zyxel.com>
2017-06-08 16:55:56 +0200

    5.3 sudo fixes

--- 5.3 Reduce sudo timeout/Config/SetSudoTimeout.sh
+++ 5.3 Reduce sudo timeout/Config/SetSudoTimeout.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
 # Script to set sudo timeout to zero.
-# Work by Allen Golbig, all credit to him. Some simplification by me.
+# Work by Allen Golbig, all credit to him.
 
-timestamp=$( grep timestamp_timeout /etc/sudoers | cut -d '=' -f 2 )
+cp /etc/sudoers /tmp/sudoers.bak
+timestamp=$(grep timestamp_timeout /tmp/sudoers.bak | cut -d '=' -f 2)
 
-if [ "$timestamp" = "" ];
-then
-	echo "Defaults timestamp_timeout=0" >> /tmp/sudoers.bak
-	visudo -csf /tmp/sudoers.bak
-	if [ $? -eq 0 ]; then
-		cp /tmp/sudoers.bak /etc/sudoers
+	if [ "$timestamp" = "" ]; then
+		echo "Defaults timestamp_timeout=0" >> /tmp/sudoers.bak
+		visudo -csf /tmp/sudoers.bak
+		if [ $? -eq 0 ]; then
+		        cp /tmp/sudoers.bak /etc/sudoers
+		fi
 	fi
-fi
 
-if [ "$timestamp" != 0 ];
-then
-	sed -i "" s/"$timestamp"/0/ /etc/sudoers
-fi
+	if [ "$timestamp" != "" ]; then
+		sed -i "" s/"$timestamp"/0/ /tmp/sudoers.bak
+		visudo -csf /tmp/sudoers.bak
+		if [ $? -eq 0 ]; then
+			cp /tmp/sudoers.bak /etc/sudoers
+		fi
+	fi
+
+rm /tmp/sudoers.bak

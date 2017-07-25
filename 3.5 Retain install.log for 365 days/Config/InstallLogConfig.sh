9688d7a34d115280d84f7df204f16d37159494be
KSchmeeds <KSchmeeds@users.noreply.github.com>
2017-07-25 09:46:46 -0500

    Update InstallLogConfig.sh
    Correct line /etc/asl/com.apple.install (was /etc/ssl/com.apple.install)

--- 3.5 Retain install.log for 365 days/Config/InstallLogConfig.sh
+++ 3.5 Retain install.log for 365 days/Config/InstallLogConfig.sh
@@ -5,7 +5,7 @@ check=$( grep -i ttl /etc/asl/com.apple.install | awk -F'ttl=' '{print $2}' )
 
 if [ "$check" = "" ];
 then
-   sed -ie 's/.format=bsd./format=bsd\ ttl='$days'/' /etc/ssl/com.apple.install
+   sed -ie 's/.format=bsd./format=bsd\ ttl='$days'/' /etc/asl/com.apple.install
 else
    sed -ie 's/.*ttl=./ttl='$days'/' /etc/asl/com.apple.install
 fi

3d6bc9cf2d4d039a5fc8dd27dbf640929f9e97cf
Richard Purves <franton@users.noreply.github.com>
2018-06-13 23:27:41 +0300

    Update SecureKB.sh
    fixed missing file extension error

--- 2.10 Secure Keyboard Entry in Terminal/Config/SecureKB.sh
+++ 2.10 Secure Keyboard Entry in Terminal/Config/SecureKB.sh
@@ -2,4 +2,4 @@
 
 user=$( python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");' )
 
-defaults write /Users/$user/Library/Preferences/com.apple.Terminal SecureKeyboardEntry -bool true
+defaults write /Users/$user/Library/Preferences/com.apple.Terminal.plist SecureKeyboardEntry -bool true
